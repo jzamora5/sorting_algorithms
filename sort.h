271b25d5e4dd5e0f2ae998c9c006ed162b56d505
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-31 16:15:14 -0500

    Shell srot with the Knuth sequence

--- sort.h
+++ sort.h
@@ -34,5 +34,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 
+/*Advanced Tasks*/
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
