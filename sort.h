7eb66714a61b74cd36bb51b32d61f2e7fdce7df1
jzamora5 <jzamora_5@yahoo.com>
2020-02-01 15:39:47 +0000

    Include Merge Sort Prototype

--- sort.h
+++ sort.h
@@ -32,9 +32,11 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
+
 
 /*Advanced Tasks*/
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
 
 #endif /* SORT_H */
