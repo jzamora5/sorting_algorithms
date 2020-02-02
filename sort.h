1ed701f3f868dd45dea5decae6445dba1f3b5ad4
jzamora5 <jzamora_5@yahoo.com>
2020-02-02 23:45:50 +0000

    ADD RADIX Sort Prototype

--- sort.h
+++ sort.h
@@ -39,5 +39,6 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 
 #endif /* SORT_H */
